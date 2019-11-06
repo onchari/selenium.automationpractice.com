67e5e7cad9031edb91195fd8c8a310fac4f649d1
OnchariWycliffe <onchariwycliffe.qa@gmail.com>
2019-11-06 14:22:03 +0300

    Added folders

--- selenium.automationpractice.com/src/test/java/automationpractice/BaseTest.java
+++ selenium.automationpractice.com/src/test/java/automationpractice/BaseTest.java
@@ -1,5 +1,13 @@
 package automationpractice;
 
-public class BaseTest {
+import org.openqa.selenium.WebDriver;
 
+public class BaseTest {
+	public static WebDriver driver;
+	public static final boolean REMOTE_DRIVER = false;
+	public static final String BROWSER = "Chrome";
+	private static final String AUT_URL = "http://automationpractice.com/";
+	private static final String SAUCELAB_USERNAME = "onchari.abuga.wycliffe";
+	private static final String SAUCELAB_ACCESSKEY = "cba0bfa9-e2bf-44ec-ba3d-bb8086ffcb14";
+	
 }
