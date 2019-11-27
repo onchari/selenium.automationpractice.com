12a45430f5b28c5c8884142c1046bc227091af18
onchari <onchariwycliffe@gmail.com>
2019-11-27 19:14:00 +0300

    Added cart functionality

---
+++ selenium.automationpractice.com/src/test/java/cartoperations/CartTest.java
@@ -0,0 +1,13 @@
+package cartoperations;
+
+import org.testng.annotations.Test;
+
+import testcommonutils.BaseTest;
+
+public class CartTest  extends BaseTest{
+
+	@Test
+	public void test1() {
+		System.out.println("Testing cart");
+	}
+}
