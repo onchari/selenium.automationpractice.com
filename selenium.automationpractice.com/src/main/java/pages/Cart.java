847bc6748bba152207f1c93da63474b3fad90c4f
onchari <onchariwycliffe@gmail.com>
2019-11-27 19:57:15 +0300

    Added Cart fuctionalities

--- selenium.automationpractice.com/src/main/java/pages/Cart.java
+++ selenium.automationpractice.com/src/main/java/pages/Cart.java
@@ -1,8 +1,5 @@
 package pages;
 
-import static org.testng.Assert.assertNotNull;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
---
+++ selenium.automationpractice.com/src/test/java/shopworkflow/CartTest.java
@@ -0,0 +1,38 @@
+package shopworkflow;
+
+import org.testng.annotations.Test;
+
+import testcommonutils.BaseTest;
+
+public class CartTest  extends BaseTest{
+
+	@Test
+	public void test1() {
+		System.out.println("Testing cart");
+	}
+	
+	@Test
+	public void deleteCartProducts() {
+		
+	}
+	
+	@Test
+	public void checkCartProductQty() {
+		
+	}
+	
+	@Test
+	public void checkCartProductPrice() {
+		
+	}
+	
+	@Test
+	public void continueToShoppingSummary() {
+		
+	}
+	
+	@Test
+	public void  increaseQtyOfProduct1() {
+		
+	}
+}
